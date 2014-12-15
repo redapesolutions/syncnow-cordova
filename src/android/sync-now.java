a0af5270dd7bc62715c9cae9f25e9e56756bc3d6
yujinlim <adamlyj89@gmail.com>
2014-12-15 15:30:11 +0800

    sync now plugin update

--- src/android/sync-now.java
+++ src/android/sync-now.java
@@ -10,7 +10,7 @@ import org.json.JSONObject;
 /**
  * This class echoes a string called from JavaScript.
  */
-public class CDVsync-now extends CordovaPlugin {
+public class SyncNow extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
