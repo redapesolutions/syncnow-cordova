abfcfa01240648c93f61f04e6fdf059bde239e63
yujinlim <adamlyj89@gmail.com>
2014-12-16 14:54:08 +0800

    add stop recording

--- www/sync-now.js
+++ www/sync-now.js
@@ -2,19 +2,28 @@ var exec = require('cordova/exec');
 var pluginName = 'SyncNow';
 
 var SyncNow = function(){
+  this.actions = {};
   var sync = this;
+  console.debug(sync);
   exec(function(actions) {
+    console.debug(actions);
     if (actions) {
       // parse actions into object
       actions.forEach(function(action) {
         sync.actions[action.toLowerCase()] = action;
       });
     }
-  }, console.error, pluginName, 'INIT', []);
+  }, function(){
+    console.error('err');
+  }, pluginName, 'INIT', []);
 };
 
 SyncNow.prototype.record = function(cb) {
   exec(cb, cb, pluginName, this.actions.record, []);
+};
+
+SyncNow.prototype.stopRecording = function(cb) {
+  exec(cb, cb, pluginName, this.actions.stop_record, []);
 };
 
 module.exports = new SyncNow();
