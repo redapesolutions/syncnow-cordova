b319023af31c00613df70ba021a59c3ba2eec67d
yujinlim <adamlyj89@gmail.com>
2014-12-16 12:34:26 +0800

    create init and record function

--- www/sync-now.js
+++ www/sync-now.js
@@ -1,5 +1,20 @@
 var exec = require('cordova/exec');
+var pluginName = 'SyncNow';
 
-exports.coolMethod = function(arg0, success, error) {
-    exec(success, error, "sync-now", "coolMethod", [arg0]);
+var SyncNow = function(){
+  var sync = this;
+  exec(function(actions) {
+    if (actions) {
+      // parse actions into object
+      actions.forEach(function(action) {
+        sync.actions[action.toLowerCase()] = action;
+      });
+    }
+  }, console.error, pluginName, 'INIT', []);
 };
+
+SyncNow.prototype.record = function(cb) {
+  exec(cb, cb, pluginName, this.actions.record, []);
+};
+
+module.exports = new SyncNow();
